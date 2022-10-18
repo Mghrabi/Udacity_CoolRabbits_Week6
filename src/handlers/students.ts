d68984e4868bebca64cdfaaffa716f86ce89c4f5
SpaceMarco_WindowsPC <black1cl0ud99@gmail.com>
2022-10-18 07:55:44 +0200

    added authentication

--- src/handlers/students.ts
+++ src/handlers/students.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from 'express';
 import { Student, StudentModel } from '../models/studentModel';
+import { authorizationMiddleWare } from '../utilities/authorization';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 
 const store = new StudentModel();
+
+dotenv.config();
 
 const index = async (_req: Request, res: Response) => {
   const users = await store.index();
@@ -22,18 +27,37 @@ const create = async (req: Request, res: Response) => {
     };
 
     const newuser = await store.create(user);
-    res.json(newuser);
+    
+    const token = jwt.sign(
+      newuser as Student,
+      process.env.TOKEN_SECRET  as string
+    );
+
+    res.json(token);
   } catch (err) {
     res.status(400);
     res.json(err);
   }
 };
-
+const authenticate = async (req: Request, res: Response) => {
+  try {
+    const user = (await store.authenticate(req.body.email, req.body.hash)) as Student;
+    const token = jwt.sign(
+      user as Student,
+      process.env.TOKEN_SECRET as string
+    );
+    res.json(token);
+  } catch (error) {
+    res.status(401);
+    res.json({ error: 'wrong password or username' });
+  }
+};
 
 const students_routes = (app: express.Application) => {
-  app.get('/users', index);
-  app.get('/users/:id', show);
-  app.post('/users', create);
+  app.get('/users', authorizationMiddleWare, index);
+  app.get('/get_user/:id',authorizationMiddleWare, show);
+  app.post('/add_users', create);
+  app.post('/authenticate', authenticate);
 };
 
 export default students_routes;
--- src/models/studentModel.ts
+++ src/models/studentModel.ts
@@ -1,4 +1,11 @@
 import client from "../db/db";
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+
+dotenv.config();
+
+const pepper = process.env.BCRYPT_PASSWORD;
+const salt_rounds = process.env.SALT_ROUNDS;
 
 export type Student = {
   id?: number;
@@ -13,10 +20,16 @@ export class StudentModel {
       const connnection = await client.connect();
       const sql =
         "INSERT INTO students(email, name, hash) VALUES ($1, $2, $3) RETURNING id, email, name;";
+
+      const hash: string = bcrypt.hashSync(
+        student.hash as string + pepper,
+        parseInt(salt_rounds as string)
+      );
+      
       const result = await connnection.query(sql, [
         student.email,
         student.name,
-        student.hash,
+        hash,
       ]);
       connnection.release();
       return result.rows[0];
@@ -65,6 +78,25 @@ export class StudentModel {
       throw new Error(
         `err in fetching student with studentId ${studentId}, err: ${err as string}`
       );
+    }
+  }
+
+  async authenticate(email: string, password: string): Promise<Student | string> {
+    try {
+      const sql = 'SELECT * FROM students WHERE email=$1';
+      const connection = await client.connect();
+
+      const res = await connection.query(sql, [email]);
+      connection.release();
+
+      if (res.rows.length) {
+        if (bcrypt.compareSync(password + pepper, res.rows[0].password)) {
+          return res.rows[0];
+        }
+      }
+      return 'wrong password or email';
+    } catch (error) {
+      throw new Error(`can\'t this.authenticate the user, Error: ${(error as Error).message}`);
     }
   }
 }
